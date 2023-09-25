15bd8a198d4fdcfc23b46b9cda76ebe80a1035e6
botus99 <botus99@gmail.com>
2023-09-25 20:42:50 +0000

    Update save-dir-list-raw.py
    fixed a typo in one of the comments

--- save-dir-list-raw.py
+++ save-dir-list-raw.py
@@ -6,7 +6,7 @@ current_dir = os.getcwd()
 # Get the list of contents in the current directory
 contents = os.listdir(current_dir)
 
-# Create a file named DirContent-pretty.txt and write the list of contents into it
+# Create a file named DirContent.txt and write the list of contents into it
 with open("DirContent.txt", "w") as file:
     for item in contents:
         file.write(item + "\n")
