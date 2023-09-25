659b1b348739a2c65056273cc254205b4d89864a
botus99 <botus99@gmail.com>
2023-09-25 13:42:35 +0000

    Add files via upload

---
+++ archival/v64-zip.bat
@@ -0,0 +1,17 @@
+@echo off
+
+REM Set the path to the folder containing the bin files
+set "folder=C:\path\to\folder"
+
+REM Navigate to the folder
+cd %folder%
+
+REM Loop through all the bin files and create a zip file for each one
+for %%f in (*.v64) do (
+    echo Creating zip file for %%f...
+    7z a -tzip "%%~nf.zip" "%%f"
+)
+
+echo Done.
+
+pause
---
+++ archival/zip-to-7z.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "outputPath=%cd%"
+
+for %%F in (*.zip) do (
+    echo 📦 Extracting "%%~nxF" with 7zip...
+    7z e -o"!outputPath!\%%~nF" "%%~fF" > nul
+    echo ✨ Compressing "%%~nxF" with 7zip...
+    7z a -t7z "!outputPath!\%%~nF.7z" "!outputPath!\%%~nF\*" > nul
+    echo 🗑️ Cleaning up...
+    rd /s /q "!outputPath!\%%~nF"
+)
+
+echo ✅ Poof! Your .zip's are .7z's now. I didn't delete the original zips. You do it.
+pause
