141649ba459e05e88e43ac3e876a41cd9a79ac55
botus99 <botus99@gmail.com>
2024-01-17 16:21:35 +0000

    update generate-plain-username.py
    clarified comment

--- generate-plain-username.py
+++ generate-plain-username.py
@@ -2,7 +2,7 @@ import random
 
 def generate_username():
 
-    # Establish the words to use for randomizing (my word choices are heavily inspired by various codenames in the Metal Gear Solid series)
+    # Establish the words to use for randomizing (my word choices are taken heavily from the various codenames in the Metal Gear Solid series)
     verbs = ['Censored', 'Climbing', 'Coding', 'Crawling', 'Decoding', 'Escaped', 'Evaded', 'Exfiltrating', 'Falling', 'Flying', 'Hacking', 'Hiding', 'Infiltrating', 'Interrogating', 'Jumping', 'Laughing', 'Obscured', 'Naked', 'Raging', 'Rescued', 'Rising', 'Running', 'Screaming', 'Sleeping', 'Sneaking', 'Spotted', 'Swimming', 'Uncensored']
     nouns = ['Aardvark', 'Alligator', 'Alpaca', 'Armadillo', 'Baboon', 'Badger', 'Bat', 'Bear', 'Beatle', 'Bison', 'Box', 'Buffalo', 'Butterfly', 'Camel', 'Cat', 'Centipede', 'Chicken', 'Cicada', 'Cobra', 'Cow', 'Crab', 'Crocodile', 'Crow', 'Cyborg', 'Deer', 'Dingo', 'Dolphin', 'Donkey', 'Eagle', 'Eel', 'Elephant', 'Falcon', 'Fox', 'Frog', 'Gerbil', 'Goat', 'Goldfish', 'Goose', 'Gorilla', 'Hawk', 'Hippo', 'Hound', 'Hyena', 'Iguana', 'Jackal', 'Jaguar', 'Koala', 'Leopard', 'Leech', 'Lion', 'Lizard', 'Llama', 'Lobster', 'Mammoth', 'Mantis', 'Mongoose', 'Mouse', 'Mule', 'Ninja', 'Octopus', 'Ocelot', 'Orca', 'Ostrich', 'Owl', 'Panda', 'Panther', 'Pelican', 'Penguin', 'Pigeon', 'Piranha', 'Puma', 'Rabbit', 'Rat', 'Raven', 'Rising', 'Scorpion', 'Seagull', 'Shark', 'Skunk', 'Sloth', 'Snake', 'Sparrow', 'Spider', 'Squirrel', 'Tarantula', 'Tiger', 'Toad', 'Turkey', 'Viper', 'Vulture', 'Walrus', 'Warthog', 'Whale', 'Wolf', 'Wasp', 'Zebra']
 
