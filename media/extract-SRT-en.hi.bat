5fe0972df55b8c3d8868971bdc303f08359be668
botus99 <botus99@gmail.com>
2024-04-14 08:38:43 -0500

    Delete media/extract-SRT-en.hi.bat

--- media/extract-SRT-en.hi.bat
+++
@@ -1,9 +0,0 @@
-# Script to extract SRT subtitles from movie files
-# Be sure to change the track number from "3" to the track number of your desired subtitle track or tracks
-
-@echo off
-
-for %%a in (*.wmv, *.avi, *.mpg, *.mpeg, *.mkv, *.mp4, *.m4v, *.flv, *.mov, *.webm) do (
-    mkvextract tracks "%%a" 3:"%%~na.en.hi.srt"
-)
-pause
