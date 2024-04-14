41dc946a82d0d08678a30694c6bcf1303be501bc
botus99 <botus99@gmail.com>
2024-04-14 08:39:15 -0500

    delete media/no-subs.bat

--- media/no-subs.bat
+++
@@ -1,8 +0,0 @@
-# Script to remove subtitles from movie files
-
-@echo off
-
-for %%f in (*.wmv, *.avi, *.mpg, *.mpeg, *.mkv, *.mp4, *.m4v, *.flv, *.mov, *.webm) do (
-    mkvmerge -o "%%~nf--no-subs.mkv" --no-subtitles "%%f"
-)
-pause
