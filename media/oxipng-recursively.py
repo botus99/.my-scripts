9d1fc5b9a2c8d1cd199f967b1ef4a29cf475ff7f
botus99 <botus99@gmail.com>
2024-04-20 09:55:43 -0500

    fixed oxipng-recursively.py
    specified the path to oxipng
    removed color (may add back later)

--- media/oxipng-recursively.py
+++ media/oxipng-recursively.py
@@ -1,31 +1,15 @@
 import os
 import subprocess
-
-# ANSI escape codes for text colors
-COLOR_GREEN = '\033[92m'
-COLOR_RED = '\033[91m'
-COLOR_RESET = '\033[0m'
-
-def process_png_file(png_file):
-    # Construct the command to run optipng
-    command = ['oxipng', png_file]
-
-    # Run the command using subprocess
-    try:
-        subprocess.run(command, check=True)
-        print(f"{COLOR_GREEN}Optimized{COLOR_RESET}: {png_file}")
-    except subprocess.CalledProcessError as e:
-        print(f"{COLOR_RED}Error optimizing{COLOR_RESET}: {png_file} - {e}")
-
-def process_directory(directory):
-    for root, _, files in os.walk(directory):
+def run_oxipng_recursively(directory):
+    # Specify the full path to the oxipng executable
+    oxipng_path = os.path.expanduser('~/.cargo/bin/oxipng')
+    
+    for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith('.png'):
-                png_file = os.path.join(root, file)
-                process_png_file(png_file)
-
-# Get the current directory
-current_directory = os.getcwd()
-
-# Process PNG files in the current directory
-process_directory(current_directory)
+                file_path = os.path.join(root, file)
+                # Use the full path to the oxipng executable in the subprocess.run call
+                subprocess.run([oxipng_path, '-o', '2', file_path])
+if __name__ == "__main__":
+    current_directory = os.getcwd()
+    run_oxipng_recursively(current_directory)
