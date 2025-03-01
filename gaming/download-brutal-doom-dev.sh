394c66697ef8faa5759bf518761e1d578c782a66
botus99 <botus99@gmail.com>
2025-03-01 06:00:31 -0600

    update brutal doom community download script

--- gaming/download-brutal-doom-dev.sh
+++ gaming/download-brutal-doom-dev.sh
@@ -1,21 +1,67 @@
 #!/usr/bin/env bash
 
-# Set the screen and text color to red
-echo -e "\e[34m\e[91m"
+# ASCII art header
+echo -e "+----------------------------------------------------------------------------------------+"
+echo -e "|\e[38;5;208m                 ██████╗  ██████╗  ██╗   ██╗ ████████╗  █████╗  ██╗                     \e[0m|"
+echo -e "|\e[38;5;208m                 ██╔══██╗ ██╔══██╗ ██║   ██║ ╚══██╔══╝ ██╔══██╗ ██║                     \e[0m|"
+echo -e "|\e[38;5;208m                 ██████╔╝ ██████╔╝ ██║   ██║    ██║    ███████║ ██║                     \e[0m|"
+echo -e "|\e[38;5;208m                 ██╔══██╗ ██╔══██╗ ██║   ██║    ██║    ██╔══██║ ██║                     \e[0m|"
+echo -e "|\e[38;5;208m                 ██████╔╝ ██║  ██║ ╚██████╔╝    ██║    ██║  ██║ ███████╗                \e[0m|"
+echo -e "|\e[38;5;208m                 ╚═════╝  ╚═╝  ╚═╝  ╚═════╝     ╚═╝    ╚═╝  ╚═╝ ╚══════╝                \e[0m|"
+echo -e "|\e[38;5;208m                                                                                        \e[0m|"
+echo -e "|\e[38;5;208m        ▓   ▓ ▓▓▓        ▓  ▓ ▓▓▓  ▓        ▓      ▓ ▓░   ░░▓  ▓▓    ▓  ▓▓              \e[0m|"
+echo -e "|\e[38;5;208m      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ░▓▓▓░▓▓▓▓▓▓▓▓░▓▒ ░░▓▓▓▓░▓▓▓▓░▓▓▓▓▓▓▓▓░▓▓▓▓░   ▓▓▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░▓▓▓▓▓▓▓  ▓▓▓▓▓░▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓▓░ ▓▓▓▓▓▓▓▓▓▓▓▓▓░▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░▓ ▓▓▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓▒    ▓▓▓▓▒▓▓▓▓▓▓▓▓▓    ▓▓▓▓▓▓▓ ▓▓▓▓▓▓░    ▓▓░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓▓    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓    ▓▓▓▓░▓▓ ▓▓▓▓▓▓░    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓▓    ▓▓▒▓▓░▓▓▓▓▓▓▓▓    ▓▓▓▓▓▓░ ▓░▓▓▓▓░    ▓▓▓▓▓▓▓▓▒▓▓▓▓▓▓▓▓▓▓▓░░▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓░▓▓    ▓▓▓░▓▓░▓▓▓▓▓░▓    ▓▓▓▓▓▓▓ ▓▓▓▓▓▓░    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓░    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓    ▓▓▓▓▓▓▓ ▓▓▓▓▓▓░    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓░    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓    ▓▓▓▓▓▓▓ ▓▓▓▓▓▓░    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓░▓▓    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓    ▓▓▓▓▓▓▓ ▓▓▓▓▓▓░    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓▓    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓    ▓ ▓▓▓▓▓ ▓▓▓▓▓▓░    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓ ▓▓▓▓▓▓░  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░▓░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓░▓▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░▓  ▓▓▓▓▓▓▓▓▓▓░▓▓▓▓▓ ▓▓▓▓▓▓░▓▓▓▓▓▓▓▓▓  ▓▓▓▓░▓▓ ░▓ ▓▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓▓▓▒▓▓▓▓▓▓░       ▓░▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓       ▓░▓▓▓    ▓▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓ ▓▓▓▓▓▓▓▓            ▓▓▓▓▓▓▓▓▓    ▓▓▓▓░▒▓▓▓▓            ▓▓    ▓▓▓░░▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓ ▓▓░                  ░▓▓░         ▓▓░▓                     ▓▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓▓▓                                                          ░▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓                                                               ▓▓▓░░       \e[0m|"
+echo -e "|\e[38;5;208m        ░▓▓                                                                   ▒▓▓       \e[0m|"
+echo -e "|\e[38;5;208m                                                                                        \e[0m|"
+echo -e "|\e[38;5;208m  ██████╗  ██████╗  ███╗   ███╗ ███╗   ███╗ ██╗   ██╗ ███╗   ██╗ ██╗ ████████╗ ██╗   ██╗\e[0m|"
+echo -e "|\e[38;5;208m ██╔════╝ ██╔═══██╗ ████╗ ████║ ████╗ ████║ ██║   ██║ ████╗  ██║ ██║ ╚══██╔══╝ ╚██╗ ██╔╝\e[0m|"
+echo -e "|\e[38;5;208m ██║      ██║   ██║ ██╔████╔██║ ██╔████╔██║ ██║   ██║ ██╔██╗ ██║ ██║    ██║     ╚████╔╝ \e[0m|"
+echo -e "|\e[38;5;208m ██║      ██║   ██║ ██║╚██╔╝██║ ██║╚██╔╝██║ ██║   ██║ ██║╚██╗██║ ██║    ██║      ╚██╔╝  \e[0m|"
+echo -e "|\e[38;5;208m ╚██████╗ ╚██████╔╝ ██║ ╚═╝ ██║ ██║ ╚═╝ ██║ ╚██████╔╝ ██║ ╚████║ ██║    ██║       ██║   \e[0m|"
+echo -e "|\e[38;5;208m  ╚═════╝  ╚═════╝  ╚═╝     ╚═╝ ╚═╝     ╚═╝  ╚═════╝  ╚═╝  ╚═══╝ ╚═╝    ╚═╝       ╚═╝   \e[0m|"
+echo -e "+----------------------------------------------------------------------------------------+"
 
-# Get the date and format it as YYYY-MM-DD
-dt=$(date +"%Y-%m-%d")
-name="Brutal_Doom"
+# Download URL
+URL="https://github.com/BLOODWOLF333/Brutal-Doom-Community-Expansion/archive/refs/heads/master.zip"
 
-echo "Downloading ${name}_${dt}"
-echo "Please Wait..."
+# Output filename
+OUTPUT_FILE="brutalv21-latest.pk3"
 
-# Use wget to download the latest staging zip and rename it
-wget --output-document "${name}_${dt}.pk3" https://github.com/BLOODWOLF333/Brutal-Doom-Community-Expansion/archive/refs/heads/master.zip
+# Check network connectivity
+if ! ping -c 1 github.com &> /dev/null; then
+    echo -e "\033[0;31mNo internet connection. \e[0mExiting."
+    exit 1
+fi
 
-# Check if the download was successful
-if [ $? -eq 0 ]; then
-    echo "Download complete."
+# Download the file using wget with a progress bar and continuing a partially downloaded file if applicable
+if ! wget --no-verbose --progress=bar --show-progress --continue --output-document="$OUTPUT_FILE" "$URL"; then
+    echo -e "\033[0;31mDownload failed. \e[0mError: $?"
+    exit 1
+fi
+
+# Check if download was successful
+if [ -f "$OUTPUT_FILE" ]; then
+    echo -e "Latest devoloper version of Brutal Doom downloaded successfully."
+    echo -e "\033[0;31mRip and tear!\e[0m"
 else
-    echo "Error: Download failed."
+    echo -e "\033[0;31mFile not found after download. \e[0mExiting."
+    exit 1
 fi
