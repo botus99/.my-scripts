4f28aa5bb4390eed7605d72f01d683da7d3597b5
botus99 <botus99@gmail.com>
2025-03-01 16:03:02 -0600

    update project brutality download script

--- gaming/download-project-brutality-staging.sh
+++ gaming/download-project-brutality-staging.sh
@@ -1,21 +1,67 @@
 #!/usr/bin/env bash
 
-# Set the screen and text color to red
-echo -e "\e[34m\e[91m"
+# ASCII art header
+echo -e "+----------------------------------------------------------------------------------------+"
+echo -e "|\e[38;5;208m        ▓   ▓ ▓▓▓        ▓  ▓ ▓▓▓  ▓        ▓      ▓ ▓░   ░░▓  ▓▓    ▓  ▓▓              \e[0m|"
+echo -e "|\e[38;5;208m      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ░▓▓▓░▓▓▓▓▓▓▓▓░▓▒ ░░▓▓▓▓░▓▓▓▓░▓▓▓▓▓▓▓▓░▓▓▓▓░   ▓▓▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░▓▓▓▓▓▓▓  ▓▓▓▓▓░▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓▓░ ▓▓▓▓▓▓▓▓▓▓▓▓▓░▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░▓ ▓▓▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓▒    ▓▓▓▓▒▓▓▓▓▓▓▓▓▓    ▓▓▓▓▓▓▓ ▓▓▓▓▓▓░    ▓▓░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓▓    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓    ▓▓▓▓░▓▓ ▓▓▓▓▓▓░    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓▓    ▓▓▒▓▓░▓▓▓▓▓▓▓▓    ▓▓▓▓▓▓░ ▓░▓▓▓▓░    ▓▓▓▓▓▓▓▓▒▓▓▓▓▓▓▓▓▓▓▓░░▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓░▓▓    ▓▓▓░▓▓░▓▓▓▓▓░▓    ▓▓▓▓▓▓▓ ▓▓▓▓▓▓░    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓░    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓    ▓▓▓▓▓▓▓ ▓▓▓▓▓▓░    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓░    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓    ▓▓▓▓▓▓▓ ▓▓▓▓▓▓░    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓░▓▓    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓    ▓▓▓▓▓▓▓ ▓▓▓▓▓▓░    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓▓    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓    ▓ ▓▓▓▓▓ ▓▓▓▓▓▓░    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓ ▓▓▓▓▓▓░  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░▓░▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓░▓▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓░▓  ▓▓▓▓▓▓▓▓▓▓░▓▓▓▓▓ ▓▓▓▓▓▓░▓▓▓▓▓▓▓▓▓  ▓▓▓▓░▓▓ ░▓ ▓▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓▓▓▒▓▓▓▓▓▓░       ▓░▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▓▓       ▓░▓▓▓    ▓▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓ ▓▓▓▓▓▓▓▓            ▓▓▓▓▓▓▓▓▓    ▓▓▓▓░▒▓▓▓▓            ▓▓    ▓▓▓░░▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓ ▓▓░                  ░▓▓░         ▓▓░▓                     ▓▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓▓▓▓                                                          ░▓▓▓▓▓▓       \e[0m|"
+echo -e "|\e[38;5;208m        ▓▓▓▓▓                                                               ▓▓▓░░       \e[0m|"
+echo -e "|\e[38;5;208m        ░▓▓                                                                   ▒▓▓       \e[0m|"
+echo -e "|\e[38;5;208m                                                                                        \e[0m|"
+echo -e "|\e[38;5;208m              ██████╗  ██████╗   ██████╗       ██╗ ███████╗  ██████╗ ████████╗          \e[0m|"
+echo -e "|\e[38;5;208m              ██╔══██╗ ██╔══██╗ ██╔═══██╗      ██║ ██╔════╝ ██╔════╝ ╚══██╔══╝          \e[0m|"
+echo -e "|\e[38;5;208m              ██████╔╝ ██████╔╝ ██║   ██║      ██║ █████╗   ██║         ██║             \e[0m|"
+echo -e "|\e[38;5;208m              ██╔═══╝  ██╔══██╗ ██║   ██║ ██   ██║ ██╔══╝   ██║         ██║             \e[0m|"
+echo -e "|\e[38;5;208m              ██║      ██║  ██║ ╚██████╔╝ ╚█████╔╝ ███████╗ ╚██████╗    ██║             \e[0m|"
+echo -e "|\e[38;5;208m              ╚═╝      ╚═╝  ╚═╝  ╚═════╝   ╚════╝  ╚══════╝  ╚═════╝    ╚═╝             \e[0m|"
+echo -e "|\e[38;5;208m                                                                                        \e[0m|"
+echo -e "|\e[38;5;208m     ██████╗  ██████╗  ██╗   ██╗ ████████╗  █████╗  ██╗      ██╗ ████████╗ ██╗   ██╗    \e[0m|"
+echo -e "|\e[38;5;208m     ██╔══██╗ ██╔══██╗ ██║   ██║ ╚══██╔══╝ ██╔══██╗ ██║      ██║ ╚══██╔══╝ ╚██╗ ██╔╝    \e[0m|"
+echo -e "|\e[38;5;208m     ██████╔╝ ██████╔╝ ██║   ██║    ██║    ███████║ ██║      ██║    ██║     ╚████╔╝     \e[0m|"
+echo -e "|\e[38;5;208m     ██╔══██╗ ██╔══██╗ ██║   ██║    ██║    ██╔══██║ ██║      ██║    ██║      ╚██╔╝      \e[0m|"
+echo -e "|\e[38;5;208m     ██████╔╝ ██║  ██║ ╚██████╔╝    ██║    ██║  ██║ ███████╗ ██║    ██║       ██║       \e[0m|"
+echo -e "|\e[38;5;208m     ╚═════╝  ╚═╝  ╚═╝  ╚═════╝     ╚═╝    ╚═╝  ╚═╝ ╚══════╝ ╚═╝    ╚═╝       ╚═╝       \e[0m|"
+echo -e "+----------------------------------------------------------------------------------------+"
 
-# Get the date and format it as YYYY-MM-DD
-dt=$(date +"%Y-%m-%d")
-name="Project_Brutality"
+# Download URL
+URL="https://github.com/pa1nki113r/Project_Brutality/archive/refs/heads/PB_Staging.zip"
 
-echo "Downloading ${name}_${dt}"
-echo "Please Wait..."
+# Output filename
+OUTPUT_FILE="project-brutality-staging.pk3"
 
-# Use wget to download the latest staging zip and rename it
-wget --output-document "${name}_${dt}.pk3" https://github.com/pa1nki113r/Project_Brutality/archive/refs/heads/PB_Staging.zip
+# Check network connectivity
+if ! ping -c 1 github.com &> /dev/null; then
+    echo -e "\033[0;31mNo internet connection. \e[0mExiting."
+    exit 1
+fi
 
-# Check if the download was successful
-if [ $? -eq 0 ]; then
-    echo "Download complete."
+# Download the file using wget with a progress bar and continuing a partially downloaded file if applicable
+if ! wget --no-verbose --progress=bar --show-progress --continue --output-document="$OUTPUT_FILE" "$URL"; then
+    echo -e "\033[0;31mDownload failed. \e[0mError: $?"
+    exit 1
+fi
+
+# Check if download was successful
+if [ -f "$OUTPUT_FILE" ]; then
+    echo -e "Latest staging version of Project Brutality downloaded successfully."
+    echo -e "\033[0;31mRip and tear!\e[0m"
 else
-    echo "Error: Download failed."
+    echo -e "\033[0;31mFile not found after download. \e[0mExiting."
+    exit 1
 fi
