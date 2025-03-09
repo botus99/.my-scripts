bc3050c857a17b3992d10f2a25f09d925c7c7deb
botus99 <botus99@gmail.com>
2025-03-09 07:03:56 -0500

    change to full path extraction

--- archival/zip-7z.sh
+++ archival/zip-7z.sh
@@ -9,7 +9,7 @@ outputPath=$(pwd)
 
 for file in *.zip; do
     echo "📦 Extracting \"$file\" with 7zip..."
-    7z e -o "$outputPath/${file%.*}" "$file" > /dev/null
+    7z x -o "$outputPath/${file%.*}" "$file" > /dev/null
     echo "✨ Compressing \"$file\" with 7zip..."
     7z a -t7z -mx=9 "$outputPath/${file%.*}.7z" "$outputPath/${file%.*}"/* > /dev/null
     echo "🗑️ Cleaning up..."
