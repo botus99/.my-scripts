ff282597c317c411a32bdd6e8ab6202026c8a519
botus99 <botus99@gmail.com>
2025-04-18 18:04:41 -0500

    add eduke32 install

--- gaming/download-duke-nukem.sh
+++ gaming/download-duke-nukem.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+flatpak install com.eduke32.EDuke32 -y
 wget --no-verbose --show-progress "https://archive.org/download/DUKE3D_DOS/DUKE3D.zip/DUKE3D%2FDUKE3D.GRP" --output-document="$HOME/.var/app/com.eduke32.EDuke32/.config/eduke32/DUKE3D.GRP"
