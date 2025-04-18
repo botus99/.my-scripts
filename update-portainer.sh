a991739162701e55dbce4c08d9073ac24cc62b7c
botus99 <botus99@gmail.com>
2025-04-18 18:08:22 -0500

    change order of operations

--- update-portainer.sh
+++ update-portainer.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
+# Pull the latest Portainer CE image
+docker pull portainer/portainer-ce:latest 
+
 # Stop and remove the existing Portainer container
 docker stop portainer 
 docker rm portainer 
-
-# Pull the latest Portainer CE image
-docker pull portainer/portainer-ce:latest 
 
 # Run the Portainer container with specified options
 docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest 
