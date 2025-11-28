0506423b05c139a140d4cbdda09d0fdf0bcc13a4
botus99 <botus99@gmail.com>
2025-11-28 14:16:03 -0600

    make script for duimon shaders

---
+++ gaming/download-duimon-shaders.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Change directory to RetroArch shaders config
+cd "$HOME"/.var/app/org.libretro.RetroArch/config/retroarch/shaders || exit 1
+
+# Create Mega_Bezel_Packs directory if it doesn't exist
+mkdir -p Mega_Bezel_Packs || exit 1
+
+# Change directory to Mega_Bezel_Packs
+cd Mega_Bezel_Packs || exit 1
+
+# Clone or pull Duimon-Mega-Bezel repository
+if [ ! -d "Duimon-Mega-Bezel" ]; then
+    git clone https://github.com/Duimon/Duimon-Mega-Bezel
+else
+    cd "Duimon-Mega-Bezel"
+    git pull
+    cd ..
+fi
+
+# Check if all commands executed successfully
+if [ $? -eq 0 ]; then
+    echo "Duimon shaders are installed and up to date."
+else
+    echo "An error occurred during execution."
+    exit 1
+fi
