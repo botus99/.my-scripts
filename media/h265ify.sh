c308ca709457e4d6addd7a94cbd1aaf5e0b92697
botus99 <botus99@gmail.com>
2025-12-02 14:33:34 -0600

    make script easier to maintain

--- media/h265ify.sh
+++ media/h265ify.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
 
 for file in ./*.mkv; do
-    ffmpeg -hide_banner -hwaccel auto -i "$file" \
-          -map 0:v -map 0:a -map_chapters 0 \
-          -pix_fmt p010le -gpu any -profile:v main10 \
-          -speed veryslow -c:v hevc_nvenc -preset:v p7 \
-          -tune:v hq -ac 2 -q:a 0.75 -rc-lookahead 32 \
-          -qmin 1 -qmax 51 -cq 27 -movflags +faststart \
+    ffmpeg \
+          -hide_banner \
+          -hwaccel auto \
+          -i "$file" \
+          -map 0:v \
+          -map 0:a \
+          -map_chapters 0 \
+          -pix_fmt p010le \
+          -gpu any \
+          -profile:v main10 \
+          -speed veryslow \
+          -c:v hevc_nvenc \
+          -preset:v p7 \
+          -tune:v hq \
+          -ac 2 \
+          -q:a 0.75 \
+          -rc-lookahead 32 \
+          -qmin 1 \
+          -qmax 51 \
+          -cq 27 \
+          -movflags +faststart \
           "${file%.mkv}.mp4"
 done
