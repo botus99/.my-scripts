c092bc1109400864e7fc5acf11af869ab9959973
botus99 <botus99@gmail.com>
2026-05-05 10:15:16 -0500

    update webcam parameters

--- media/video/webcam.sh
+++ media/video/webcam.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
-#
-# open webcam in mpv
 
-mpv --demuxer-lavf-o=video_size=1280x720,input_format=rawvideo av://v4l2:/dev/video0 --profile=low-latency
+# reference link: https://mpv.io/manual/master/#low-latency-playback
+
+# fix for vapoursynth issue
+#unset LD_LIBRARY_PATH
+
+# open webcam in mpv
+mpv av://v4l2:/dev/video0 \
+  --demuxer-lavf-o=video_size=1280x720,input_format=mjpeg \
+  --profile=low-latency \
+  --opengl-glfinish=yes \
+  --opengl-swapinterval=0 \
+  --untimed \
+  --framedrop=no \
+  --speed=1.01 \
+  --really-quiet \
+  --no-terminal \
+  --force-window=yes \
+  --container-fps-override=60
