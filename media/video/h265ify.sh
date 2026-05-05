767ffea7c95def789a0a542542dfdaee37612003
botus99 <botus99@gmail.com>
2026-05-05 10:14:46 -0500

    add header text

--- media/video/h265ify.sh
+++ media/video/h265ify.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
+##=============================================================================#
+# name:        h265ify.sh
+# author:      botus99
+# updated    : 2026-05-05
+# description: reencode all mkv files in current directory to mp4 using...
+#               - nvenc encoder
+#               - 10-bit color depth
+#               - variable bitrate (constant quality)
 #
 # some common video filters: hqdn3d, deblock, deband (add after deinterlacing if using these)
 # here are some deinterlacing filters to add when needed...
 #           -vf bwdif \ # Uncomment for frame-doubling bob de-interlacing
 #           -vf yadif=1 \ # Uncomment for frame-doubling yadif de-interlacing
 #           -vf fieldmatch,decimate,fps=24000/1001 \ # Uncomment for converting 29.97fps (TV) to 23.976fps (film)
+#=============================================================================#
 
 for file in ./*.mkv; do
     ffmpeg \
