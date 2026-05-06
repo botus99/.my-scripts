68bbb1aa0e727292ec766cb74965174deb6fd708
botus99 <botus99@gmail.com>
2026-05-06 09:59:02 -0500

    prevent needless updates

--- system/update-portainer.sh
+++ system/update-portainer.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env bash
 
-# Pull the latest Portainer CE image
-docker pull portainer/portainer-ce:latest 
+# exit if things get weird
+set -e
 
-# Stop and remove the existing Portainer container
-docker stop portainer 
-docker rm portainer 
+# pull image and capture output
+PULL_OUTPUT=$(sudo docker pull portainer/portainer-ce:latest)
 
-# Run the Portainer container with specified options
-docker run -d -p 8000:8000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest 
+# if docker pulls a new image, restart portainer
+if ! echo "$PULL_OUTPUT" | grep -q "Image is up to date"; then
+  echo "New image detected. Restarting Portainer..."
 
-# Remove unused Docker images to free up disk space and prevent clutter
-docker image prune -f
+  sudo docker stop portainer
+  sudo docker rm portainer
+  sudo docker run -d \
+    -p 8000:8000 \
+    -p 9443:9443 \
+    --name=portainer \
+    --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data \
+    "$IMAGE"
+
+  sudo docker image prune -f
+else
+  echo "Portainer image is already up to date. Nothing to do."
+fi
