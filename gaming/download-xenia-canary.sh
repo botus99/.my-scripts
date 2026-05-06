70f91e50cfa3b806ceac3e8432cdd9895cf7e6c9
botus99 <botus99@gmail.com>
2026-05-06 09:59:37 -0500

    small cleanup

--- gaming/download-xenia-canary.sh
+++ gaming/download-xenia-canary.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 
-# Exit if anything fails
+# exit if anything fails
 set -euo pipefail
 
-# Set directory for Xenia variable
+# set directory for Xenia variable
 XENIADIR="$HOME/xenia-canary"
 
-# Check network connectivity
+# check network connectivity
 if ! ping -c 1 github.com &> /dev/null; then
     echo -e "\033[0;31mNo internet connection. \e[0mExiting."
     exit 1
 fi
 
-# Make sure Xenia directory exists
-mkdir "$XENIADIR"
+# make sure Xenia directory exists
+mkdir -p "$XENIADIR"
 
-# Download the latest Xenia release (Windows)
+# download latest Xenia release (linux)
+#wget -q --show-progress --progress=bar -c "https://github.com/xenia-canary/xenia-canary/releases/download/latest/xenia_canary_linux.AppImage"
+
+# download latest Xenia release (windows)
 wget -q --show-progress --progress=bar -c "https://github.com/xenia-canary/xenia-canary-releases/releases/latest/download/xenia_canary_windows.zip"
 
-# Download the latest Xenia release (Linux)
-#wget -q --show-progress --progress=bar -c "https://github.com/xenia-canary/xenia-canary-releases/releases/latest/download/xenia_canary_linux.tar.gz"
-
-# Check if the download was successful
+# check if download was successful
 if [ $? -eq 0 ]; then
     echo "Xenia Canary Windows release successfully downloaded."
 else
@@ -30,10 +30,10 @@ else
     exit 1
 fi
 
-# Unzip the downloaded file
+# unzip downloaded file
 unzip xenia_canary_windows.zip -d "$XENIADIR"
 
-# Clean up
+# clean up
 rm xenia_canary_windows.zip
 
 echo "Xenia Canary download and extraction completed successfully!"
